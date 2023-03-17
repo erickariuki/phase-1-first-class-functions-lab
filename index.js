0dbb02b1b9f8b25e16b25ead63ec55360f1d060d
erickariuki <eric.kariuki@student.moringaschool.com>
2023-03-17 15:23:38 +0300

    -completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const drivers=['Stacy', 'Eric', 'Kanambo', 'Johnie']
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2)
+}
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2)
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+function createFareMultiplier(integer) {
+    return function(fare) {
+        return fare*integer;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(drivers, selectingDrivers) {
+    return selectingDrivers(drivers);
+}
